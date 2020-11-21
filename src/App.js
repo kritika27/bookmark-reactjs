47a363efab03c74ba2074ce3a5bd2a9b2af288b6
kritika <kritikasri27@gmail.com>
2020-11-21 17:28:10 +0530

    Added main logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Item from "./components/Item";
+import { v4 as uuidv4 } from "uuid";
+import validator from "validator";
+
+const arr = () => {
+  let data = localStorage.getItem("url");
+  if (data) return JSON.parse(localStorage.getItem("url"));
+  else return [];
+};
 
 function App() {
+  const [item, setItem] = useState("");
+  const [url, setUrl] = useState("");
+  const [list, setList] = useState(arr);
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    const newItem = {
+      id: uuidv4(),
+      item: item,
+      url: url,
+    };
+    e.preventDefault();
+    if (item && url) {
+      setError("");
+      if (validator.isURL(url)) {
+        setList([...list, newItem]);
+        setItem("");
+        setUrl("");
+      } else setError("Enter the correct url format.");
+    } else {
+      setError("Enter both the Required fields.");
+    }
+  };
+
+  React.useEffect(() => {
+    localStorage.setItem("url", JSON.stringify(list));
+  }, [list]);
+
+  const handleChange = (e) => {
+    setItem(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>BOOKMARKS</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <label>Website Name</label>
+        <input type="text" value={item} onChange={handleChange} />
+        <br></br>
+        <label>Website Url</label>
+        <input
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <p style={{ color: "red" }}>{error}</p>
+        <button className="btn" type="submit">
+          Add Bookmark
+        </button>
+        <br></br>
+        <br></br>
+      </form>
+      <div>
+        {list.map((c, id) => (
+          <Item
+            key={id}
+            id={c.id}
+            item={c.item}
+            url={c.url}
+            list={list}
+            setList={setList}
+            setItem={setItem}
+          />
+        ))}
+      </div>
     </div>
   );
 }
